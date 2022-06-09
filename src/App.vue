6621cffb302d0278440644eab3c4fe7d096c2a8e
남상우 <>
2022-06-09 12:41:47 +0900

    express add

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,65 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img :src="captureData" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import html2canvas from 'html2canvas'
 
 export default {
   name: 'App',
+  metaInfo: {
+    // if no subcomponents specify a metaInfo.title, this title will be used
+    title: 'Default Title',
+    // all titles will be injected into this template
+    meta: [
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: '타이틀내용입니다.'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: '설명내용입니다.'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'http://placeimg.com/640/480/fashion'
+      },
+      {
+        hid: 'title',
+        property: 'title',
+        content: '타이틀내용입니다.'
+      },
+      {
+        hid: 'description',
+        property: 'description',
+        content: '설명내용입니다.'
+      },
+    ]
+  },
+  data(){
+    return{
+      captureData: ''
+    }
+  },
   components: {
     HelloWorld
+  },
+  mounted() {
+    this.getScreenshot()
+  },
+  methods: {
+    getScreenshot() {
+      html2canvas(document.querySelector("#app")).then(canvas => {
+        this.captureData= canvas.toDataURL()
+      });
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Meta from 'vue-meta'
 Vue.config.productionTip = false
+
+Vue.use(Meta, {
+  keyName: 'metaInfo',
+  attribute: 'data-vue-meta',
+  ssrAttribute: 'data-vue-meta-server-rendered',
+  tagIDKeyName: 'vmid',
+  refreshOnceOnNavigation: true
+})
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
